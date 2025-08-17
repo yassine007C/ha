decf10538d81067a53684f939e7ae545fc9b1fd0
yassine007C <104086968+yassine007C@users.noreply.github.com>
2025-08-17 17:52:52 +0100

    Update hgf2.py

--- hgf2.py
+++ hgf2.py
@@ -45,14 +45,18 @@ st.markdown("""
     }
     </style>
     <div class="top-button-container">
-        <a class="top-button" href="https://example.com" target="_blank">
-            <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png">
+        <a class="top-button" href="https://ko-fi.com/azolimgod" target="_blank">
+            <img src="https://storage.ko-fi.com/cdn/logomarkLogo.png">
             <span>Go to Site</span>
         </a>
     </div>
 """, unsafe_allow_html=True)
 
 
+st.markdown("""
+   <script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support me on Ko-fi', '#72a4f2', 'W7W21JUJGD');kofiwidget2.draw();</script> 
+ 
+""", unsafe_allow_html=True)
 
 
    
